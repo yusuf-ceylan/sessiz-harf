8a229c4229c5daff6c7565c6d5e438b5a0b8d285
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-22 18:44:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace sessiz_harf
+{
+    class Program
+    {
+        static bool IsConsonant(char c)
+        {
+            char[] consonants = { 'b', 'c', 'ç', 'd', 'f', 'g', 'ğ', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z' };
+            return Array.Exists(consonants, x => x == char.ToLower(c));
+        }
+
+        static bool CheckForConsecutive(string text)
+        {
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                if (IsConsonant(text[i]) && IsConsonant(text[i + 1]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metninizi girin: ");
+            string input = Console.ReadLine();
+
+            string[] inputArray = input.Split(' ');
+
+            foreach (var item in inputArray)
+            {
+                bool result = CheckForConsecutive(item);
+                Console.Write(result ? "True " : "False ");
+            }
+        }
+    }
+}
